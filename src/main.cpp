5d69002757c1d8c6f570a412dfcc71f6eed313dc
bignut <1214002133@qq.com>
2023-09-20 20:12:30 +0800

    std::max_element

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,61 @@
 #include <gtest/gtest.h>
 #include <vector_expression.h>
 
-// template<typename T>
-// class A{
-//     const T& operator()(){return static_cast<const T&>(*this);}
-// };
-
-// class B : public A<B> {
-//     public:
-//         B(const int &a, const int &b){}
-//     void func(){std::cout<<"hhh"<<std::endl;}
-// };
-// template<typename T>
-// B operator+(const A<T>& a, const A<T> &b)
-// {
-//     return B()
-// }
-
-int a[] = {1, 2, 3, 4, 5, 6};
-
+int a[] = {1, 2, 3, 4, 5, 6, 7};
 int *begin = std::begin(a);
 int *end = std::end(a);
 
-bool func(int a, int b){return a < b;}
+bool func(int &a, int &b)
+{
+    return a < b;
+}
+
+namespace big
+{
+    template <typename _Compare>
+    struct _Iter_comp_iter
+    {
+        _Compare _M_comp;
+
+        _Iter_comp_iter(_Compare __comp) : _M_comp(std::move(__comp)) {}
+
+        template <typename _Iter1, typename _Iter2>
+        bool operator()(_Iter1 __it1, _Iter2 __it2)
+        {
+            return bool(_M_comp(*__it1, *__it2));
+        }
+    };
+
+
+    template <typename _Compare>
+    _Iter_comp_iter<_Compare> _iter_comp_iter(_Compare __comp)
+    {
+        return _Iter_comp_iter<_Compare>(std::move(__comp));//为啥要move一下呢？？
+    }
+
+
+    template <typename _Iter, typename _Comp>
+    _Iter __max_element(_Iter __first, _Iter __end, _Comp __comp)
+    {
+        if(__first == __end) return __first;
+        _Iter __result = __first;
+        while(++__first != __end)
+            if (__comp(__result, __first))
+                __result = __first;
+        return __result;
+    }
+
+    template <typename _Iter, typename _Comp>
+    _Iter max_element(_Iter __first, _Iter __end, _Comp __comp)
+    {
+        return __max_element(__first, __end, _iter_comp_iter(__comp));
+    }
+}
 
 int main(int argc, char **argv)
 {
-
-    int *iter = std::max_element(begin, end, func);
-    std::cout<<*iter<<std::endl;
-    big::Array3<int> arr(3, 3, 3, 10);
-    big::Array3<int> add(3, 3, 3, 9);
-
-    int scal = 5;
-
-    // TEST();
+    int * a = big::max_element(begin, end, func);
+    std::cout << *a << std::endl;
+    // big::Vector<int, 4> aaa(1,2,3,4);
     return 0;
 }
